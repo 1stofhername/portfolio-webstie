d2c8c9f130d3459bf41571918eee83ec328f19ac
Alicia <arrowsrp@yahoo.com>
2022-05-23 17:33:21 -0700

    Adjusts styling of contact menu

--- src/components/ProjectsContainer.js
+++ src/components/ProjectsContainer.js
@@ -25,6 +25,7 @@ export default function Projects () {
                     
                     {projectData.map((project)=>{
                         return (
+                            
                         <ProjectCard project={project} />
                         )})}
                         
