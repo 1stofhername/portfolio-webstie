8e99161ad60cf7c76b82bcf64abc510eaad7be7a
Alicia <arrowsrp@yahoo.com>
2022-05-25 20:07:25 -0700

    Deletes unused vars and adds alt text to images.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,29 +1,23 @@
-import { Link, useLocation } from "react-router-dom";
-import { useState } from "react";
 import user from '../assets/icons/user.svg';
-import activeUser from '../assets/icons/activeUser.svg';
 import work from '../assets/icons/work.svg';
 import build from '../assets/icons/build.svg';
 
 export default function Navbar () {
-
-    const [activeLink, setActiveLink]=useState("")
-    const { pathname } = useLocation();
 
     return (
         
         <div id="internal-links">
             
             <Link to="/" className="nav-link">
-                <img id="home-page-link" className="nav-icon" src={user} />
+                <img id="home-page-link" className="nav-icon" src={user} alt="home-link" />
             </Link>
             
             <Link to="/resume" className="nav-link" id="resume-page-link">
-                <img id="resume-page-link" className="nav-icon" src={work}/>
+                <img id="resume-page-link" className="nav-icon" src={work} alt="resume-link" />
             </Link>
             
             <Link to="/projects" className="nav-link" id="projects-page-link">
-                <img id="projects-page-link" className="nav-icon" src={build} />
+                <img id="projects-page-link" className="nav-icon" src={build} alt="projects-link" />
             </Link>
         </div>
     )
--- src/data/project-data.js
+++ src/data/project-data.js
@@ -10,7 +10,7 @@ export const projectData = [
                 readme:"https://github.com/1stofhername/Phase1-Project/blob/main/README.md"
             },
         ],
-        tools:["Vanilla Javascript", "OOP", "JSON"],
+        tools:["Vanilla Javascript", "OOP", "JSON", "CSS3"],
     },
     {
         title: "Idealist",
@@ -23,7 +23,7 @@ export const projectData = [
                 readme:"https://github.com/1stofhername/deployed-idea-list/blob/main/README.md"
             }
         ],
-        tools:["Javascript", "React.js", "JSX", "React-router-dom", "CSS", "Netlify"]
+        tools:["Javascript", "React.js", "JSX", "React Router", "CSS3", "Netlify"]
 
     },
 
@@ -38,7 +38,7 @@ export const projectData = [
                 readme:"https://github.com/1stofhername/portfolio-webstie/blob/main/README.md"
             }
         ],
-        tools:["Javascript", "React.js", "JSX", "React-router-dom", "CSS", "Framer motion", "NoSQL"]
+        tools:["Javascript", "React.js", "JSX", "React Router", "CSS3", "Framer motion", "NoSQL"]
 
     }
 ]
