6b3e56433d1670bcd12c359c0a9487638ebdf3a8
Alicia <arrowsrp@yahoo.com>
2022-05-25 20:21:57 -0700

    Adds alt text to contact menu images

--- src/components/AnimatedRoutes.js
+++ src/components/AnimatedRoutes.js
@@ -1,22 +1,12 @@
-import { Route, Routes, useLocation } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import { AnimatePresence } from 'framer-motion';
-import { useState } from 'react';
 import Home from './Home';
 import ExperienceContainer from './ExperienceContainer';
 import ProjectsContainer from './ProjectsContainer';
 import ExperienceDetails from './ExperienceDetails';
-import { roles } from '../data/job-data';
 import Sidebar from './Sidebar';
 
 export default function AnimatedRoutes () {
-
-  function handleJobClick (org) {
-    let job = roles.filter((role)=>role.org===org)
-    setSelectedJob(job);
-  }
-
-  const[selectedJob, setSelectedJob]=useState("");
-  const location = useLocation({});
 
     return (
     
@@ -24,7 +14,7 @@ export default function AnimatedRoutes () {
       <Sidebar />
       <AnimatePresence exitBeforeEnter>
         <Routes location={location} key={location.pathname}>
-          <Route path="/resume" element={<ExperienceContainer handleJobClick={handleJobClick}/>} />
+          <Route path="/resume" element={<ExperienceContainer />} />
           <Route path="/projects" element={<ProjectsContainer />} />
           <Route path="/resume/:org" element={<ExperienceDetails />} />
           <Route path="/" element={<Home />} />
