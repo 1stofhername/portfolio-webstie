76630f102182ec676a7faab40456ed3e5ff65cd9
Alicia Parris <alicia@Honeycrisp-2.local>
2024-03-26 00:22:38 -0700

    Updates resume link.

--- src/components/ExperienceContainer.js
+++ src/components/ExperienceContainer.js
@@ -18,7 +18,7 @@ export default function Resume () {
             exit={{ y: window.innerHeight, transition: { duration: 1.0 } }}
         >
             <div className="page-title">
-                <a  className="pdf-link" href="https://docs.google.com/document/d/e/2PACX-1vRTxL47yONAu1zHoo8XYkoSX1ZLs8Ox1XvI5JzzDzaYkMKd4ntjD6YiWAx1EYYRD0Bk5EYGkECl9h76/pub">
+                <a  className="pdf-link" href="https://docs.google.com/document/d/e/2PACX-1vSTTzXoXU8aBTKpSIvw5uPL0UpjrsdWDsljymNaHzrhgRcdkQX_p8_Wn4ylZR0_qMEJmEiAz4GJ5M5f/pub">
                     <img className="pdf-icon" src={pdf} alt="resume pdf" />
                 </a>
                 <h1 className="sub-title-black">
